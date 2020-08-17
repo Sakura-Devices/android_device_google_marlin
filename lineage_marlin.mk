a65b564886469d090ebed4e3295ae926c4c5c3cb
2gracz <andrzej.lenarczyk.5@gmail.com>
2020-08-17 17:12:34 +0200

    Marlin: Add Gapps

--- lineage_marlin.mk
+++ lineage_marlin.mk
@@ -1,6 +1,7 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOTANIMATION_RES := 1440
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -24,3 +25,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/marlin/marlin:8.1.0/OPM1.171019.021/4565141:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
+
+#Sakura Gapps  
+SAKURA_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
